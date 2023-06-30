cf4292cae50ad0c0417b24c6540afa470c8f6e8a
vorotyncev <vorotyncev.si@gmail.com>
2023-06-30 17:46:33 +0300

    fixed-conftest

--- conftest.py
+++ conftest.py
@@ -2,25 +2,23 @@ import pytest
 import datetime
 from django.conf import settings
 from datetime import datetime, timedelta
-# Импортируем модель заметки, чтобы создать экземпляр.
 from news.models import News, Comment
 
 
 @pytest.fixture
-# Используем встроенную фикстуру для модели пользователей django_user_model.
 def author(django_user_model):
     return django_user_model.objects.create(username='Автор')
 
 
 @pytest.fixture
-def author_client(author, client):  # Вызываем фикстуру автора и клиента.
-    client.force_login(author)  # Логиним автора в клиенте.
+def author_client(author, client):
+    client.force_login(author)
     return client
 
 
 @pytest.fixture
 def news(author):
-    news = News.objects.create(  # Создаём объект заметки.
+    news = News.objects.create(
         title='Заголовок',
         text='Текст ',
     )
@@ -41,7 +39,10 @@ def comment(author, news):
 def all_news(admin_client):
     today = datetime.today()
     all_news = [
-        News(title=f'Новость {index}', text='Просто текст.', date=today - timedelta(days=index))
+        News(
+            title=f'Новость {index}',
+            text='Просто текст.',
+            date=today - timedelta(days=index))
         for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
     ]
     return News.objects.bulk_create(all_news)
